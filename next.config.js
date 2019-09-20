f9dc1aac7a14877a587b44c316d6af4a6aaad7cc
Forte <hedikasmanto@gmail.com>
2019-09-20 15:13:17 +0700

    Fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,34 +1,30 @@
-const withTM = require("next-transpile-modules");
-const withPlugins = require("next-compose-plugins");
-var nodeExternals = require("webpack-node-externals");
+const withTM = require('next-transpile-modules')
 
-module.exports = withPlugins([
-  {
-    webpack: (config, { defaultLoaders, isServer, dev }) => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: "empty"
-      };
-
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-        // Transform all direct `react-native` imports to `react-native-web`
-        "react-native$": "react-native-web"
-      };
-
-      defaultLoaders.babel.options.plugins = [
-        ["react-native-web", { commonjs: true }]
-      ];
-
-      /*******************************************
-       *** Externals
-       ******************************************/
-      // Configure externals
-      return config;
+module.exports = withTM({
+    transpileModules: [
+      "react-native", "styled-components", "styled-components/native"
+    ],
+    webpack: config => {
+      return {
+        ...config,
+        resolve: {
+          ...config.resolve,
+          extensions: [
+            '.web.ts',
+            '.web.tsx',
+            '.ts',
+            '.tsx',
+            '.web.js',
+            '.web.jsx',
+            '.js',
+            '.jsx',
+            ...config.resolve.extensions
+          ],
+          alias: {
+            ...config.resolve.alias,
+            'react-native$': 'react-native-web'
+          }
+        }
+      }
     }
-  },
-  [
-    withTM,
-    ["react-native-web", "styled-components", "styled-components/native"]
-  ]
-]);
+  })
